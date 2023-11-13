330213770b2a69a2789e0bb3b3f53858ebe9062e
tran quoc huy <tranquochuy15042003@gmail.com>
2023-11-13 15:46:09 +0700

    sửa lại regex

--- src/app/Frm_KhachHang.java
+++ src/app/Frm_KhachHang.java
@@ -57,7 +57,7 @@ public class Frm_KhachHang extends JFrame implements ActionListener, MouseListen
 
 	public Frm_KhachHang() throws SQLException {
 		// getContentPane().setBackground(Color.CYAN);
-		setTitle("QUẢN LÝ Khách Hàng");
+		setTitle("QUẢN LÝ KHÁCH HÀNG");
 		setSize(1400, 670);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
@@ -78,7 +78,7 @@ public class Frm_KhachHang extends JFrame implements ActionListener, MouseListen
 		panel = new JPanel();
 
 		panel.setBorder(new LineBorder(new Color(0, 0, 0), 3));
-	    panel.setBackground(new Color(207, 169, 0));	
+		panel.setBackground(new Color(207, 169, 0));
 
 		panel.setBounds(102, 51, 1200, 271);
 		pnQLKH.add(panel);
@@ -125,7 +125,7 @@ public class Frm_KhachHang extends JFrame implements ActionListener, MouseListen
 		lblLoaiKhachHgang.setBounds(79, 143, 81, 36);
 		panel.add(lblLoaiKhachHgang);
 
-		lblDTL = new JLabel("ĐIểm tích luỹ:");
+		lblDTL = new JLabel("Điểm tích luỹ:");
 		lblDTL.setFont(new Font("Tahoma", Font.BOLD, 17));
 		lblDTL.setForeground(new Color(255, 255, 255));
 		lblDTL.setBounds(679, 141, 123, 41);
@@ -164,7 +164,7 @@ public class Frm_KhachHang extends JFrame implements ActionListener, MouseListen
 		lbTB = new JLabel();
 		lbTB.setHorizontalAlignment(SwingConstants.LEFT);
 		lbTB.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lbTB.setBounds(400, 178, 500, 36);
+		lbTB.setBounds(380, 180, 700, 36);
 		lbTB.setForeground(Color.RED);
 		panel.add(lbTB);
 
@@ -267,15 +267,15 @@ public class Frm_KhachHang extends JFrame implements ActionListener, MouseListen
 			if (btnThem.getText().equalsIgnoreCase("Thêm")) {
 				btnThem.setText("Xác nhận");
 				btnSua.setText("Huỷ");
-			}  else if (btnThem.getText().equalsIgnoreCase("Xác nhận")) {
-				if(themKH()) {
-				btnSua.setText("Sửa");
-				btnThem.setText("Thêm");
+			} else if (btnThem.getText().equalsIgnoreCase("Xác nhận")) {
+				if (themKH()) {
+					btnSua.setText("Sửa");
+					btnThem.setText("Thêm");
 				}
 			} else if (btnThem.getText().equals("Xác nhận ")) {
-				if(suaKH()) {
-				btnThem.setText("Thêm");
-				btnSua.setText("Sửa");
+				if (suaKH()) {
+					btnThem.setText("Thêm");
+					btnSua.setText("Sửa");
 				}
 			}
 		} else if (o.equals(btnSua)) {
@@ -418,7 +418,8 @@ public class Frm_KhachHang extends JFrame implements ActionListener, MouseListen
 	public boolean ktraDuLieu() {
 
 		String ten = txtTenKH.getText();
-		if (ten.equals("") || !ten.matches("^[A-ĐĐ-Ỹ][a-đđ-ỹ]*( [A-ĐĐ-Ỹ][a-đđ-ỹ]*)*$")) {
+		if (ten.equals("") || !ten.matches(
+				"^[A-Za-za-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ]+$")) {
 			showMessage("(*) Tên không được để trống và viết hoa chữ cái đầu");
 			txtTenKH.requestFocus();
 			return false;
@@ -431,8 +432,8 @@ public class Frm_KhachHang extends JFrame implements ActionListener, MouseListen
 				return false;
 			}
 		}
-		if (dt.equals("") || !dt.matches("\\d{10}")) {
-			showMessage("(*)Số điện thoại không để trống và chỉ được 10 số");
+		if (dt.equals("") || !dt.matches("^(0[0-9]{9})$")) {
+			showMessage("(*)Số điện thoại không để trống và chỉ được 10 số, bắt đầu bằng số 0");
 			txtSDT.requestFocus();
 			return false;
 		}
@@ -449,8 +450,7 @@ public class Frm_KhachHang extends JFrame implements ActionListener, MouseListen
 		}
 		return true;
 	}
-	
-	
+
 	@Override
 	public void mouseClicked(MouseEvent e) {
 
